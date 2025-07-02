633021d1a4972353a4a33dbac959327bbedfbb3d
aman-singh122 <ranjannikhil334@gmail.com>
2025-07-02 12:29:35 +0530

    Updated project

--- index.cjs
+++ index.cjs
@@ -133,7 +133,7 @@ Respond ONLY in this format:
           console.log("AI raw response:", response.text);
           let aiText = response.text.trim();
           // Remove code block markers if present
-          if (aiText.startsWith("```") ) {
+          if (aiText.startsWith("```")) {
             aiText = aiText
               .replace(/^```(?:json)?/i, "")
               .replace(/```$/, "")
